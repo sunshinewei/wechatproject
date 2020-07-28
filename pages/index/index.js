9ca8ad61e1845b76631252d0dc3858da0e8bf09e
weilong <1436699184@qq.com>
2020-07-28 17:03:40 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,73 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  subway:function(){
+    let plugin = requirePlugin("subway");
+    let key = '生活工具-小程序';//使用在腾讯位置服务申请的key;
+    let referer = '生活工具'; //调用插件的app的名称
+    wx.navigateTo({
+        url: 'plugin://subway/index?key=' + key + '&referer=' + referer
+      });
+    },
+
+
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+  }
+
+  
+
+
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+  // },
+  // getUserInfo: function(e) {
+  //   console.log(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
+
+  
+})
